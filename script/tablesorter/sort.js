699db9952601488672c1d5ed1766435b7d922bdf
NewGeib <iviviv1901@gmail.com>
2015-02-19 21:21:10 +0200

    Оптимізація

--- script/tablesorter/sort.js
+++ script/tablesorter/sort.js
@@ -1,8 +1,3 @@
-// сортировка таблицы
-      // использовать делегирование!
-      // должно быть масштабируемо:
-      // код работает без изменений при добавлении новых столбцов и строк
-
       var grid = document.getElementById('inp-table');
 
       grid.onclick = function(e) {
@@ -11,17 +6,17 @@
         if (target.tagName != 'TH') return;
 
         // Если TH -- сортируем
-				$('#myList tr:lt('+x+')').attr("hidden", "hidden");
+				$('#myList').find('tr:lt('+x+')').attr("hidden", "hidden");
         sortGrid(target.cellIndex, target.getAttribute('data-type'));
-				$('#myList tr:lt('+x+')').removeAttr("hidden");
+				$('#myList').find('tr:lt('+x+')').removeAttr("hidden");
       };
 
       function sortGrid(colNum, type) {
         var tbody = grid.getElementsByTagName('tbody')[0];
 
         // Составить массив из TR
-        var rowsArray = [];
-        for(var i = 0; i<tbody.children.length; i++) {
+        var rowsArray = [], i;
+        for(i = 0; i<tbody.children.length; i++) {
           rowsArray.push(tbody.children[i]);
         }
 
@@ -30,41 +25,57 @@
 
         switch(type) {
           case 'number':
-						if(document.getElementById('sorted') !== null && document.getElementById('sorted').cellIndex == colNum) {
-							compare = function(rowA, rowB) {
-		            return rowB.cells[colNum].innerHTML - rowA.cells[colNum].innerHTML;
-		          };
-							document.getElementById('sorted').removeAttribute("id");
-							//grid.tHead.rows[0].cells[colNum].id = 'sorted';
-						}	else {
-				      compare = function(rowA, rowB) {
-				        return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
-				      };
-							if(document.getElementById('sorted') !== null)document.getElementById('sorted').removeAttribute("id");
-							grid.tHead.rows[0].cells[colNum].id = 'sorted';
-						}
+            if(document.getElementById('sorted') !== null && document.getElementById('sorted').cellIndex == colNum) {
+                compare = function (rowA, rowB) {
+                    var a = rowA.cells[colNum].innerHTML;
+                    var b = rowB.cells[colNum].innerHTML;
+                    if(a.charAt(a.length-1) == '%'){a=a.substring(0, a.length-1);}
+                    if(b.charAt(b.length-1) == '%'){b=b.substring(0, b.length-1);}
+                    return b - a;
+                };
+                document.getElementById('sorted').removeAttribute("id");
+                //grid.tHead.rows[0].cells[colNum].id = 'sorted';
+            }	else {
+                compare = function (rowA, rowB) {
+                    var a = rowA.cells[colNum].innerHTML;
+                    var b = rowB.cells[colNum].innerHTML;
+                    if(a.charAt(a.length-1) == '%'){a=a.substring(0, a.length-1);}
+                    if(b.charAt(b.length-1) == '%'){b=b.substring(0, b.length-1);}
+                    return a - b;
+                };
+                if (document.getElementById('sorted') !== null)document.getElementById('sorted').removeAttribute("id");
+                grid.tHead.rows[0].cells[colNum].id = 'sorted';
+            }
             break;
           case 'string':
-						if(document.getElementById('sorted') !== null && document.getElementById('sorted').cellIndex == colNum) {
-		          compare = function(rowA, rowB) {
-								var str1 = rowA.cells[colNum].innerHTML.toLowerCase();
-								var str2 = rowB.cells[colNum].innerHTML.toLowerCase();
-								if(str1.charAt(0) == '"'){str1 = str1.substr(1);}
-								if(str2.charAt(0) == '"'){str2 = str2.substr(1);}
-		            return str1 > str2 ? -1 : 1;
-		          };
-							document.getElementById('sorted').removeAttribute("id");
-						}	else {
-							compare = function(rowA, rowB) {
-								var str1 = rowA.cells[colNum].innerHTML.toLowerCase();
-								var str2 = rowB.cells[colNum].innerHTML.toLowerCase();
-								if(str1.charAt(0) == '"'){str1 = str1.substr(1);}
-								if(str2.charAt(0) == '"'){str2 = str2.substr(1);}
-		            return str1 > str2 ? 1 : -1;
-		          };
-							if(document.getElementById('sorted') !== null)document.getElementById('sorted').removeAttribute("id");
-							grid.tHead.rows[0].cells[colNum].id = 'sorted';
-						}
+                if(document.getElementById('sorted') !== null && document.getElementById('sorted').cellIndex == colNum) {
+                    compare = function (rowA, rowB) {
+                        var str1 = rowA.cells[colNum].innerHTML.toLowerCase();
+                        var str2 = rowB.cells[colNum].innerHTML.toLowerCase();
+                        if (str1.charAt(0) == '"') {
+                            str1 = str1.substr(1);
+                        }
+                        if (str2.charAt(0) == '"') {
+                            str2 = str2.substr(1);
+                        }
+                        return str1 > str2 ? -1 : 1;
+                    };
+                    document.getElementById('sorted').removeAttribute("id");
+                }	else {
+                    compare = function (rowA, rowB) {
+                        var str1 = rowA.cells[colNum].innerHTML.toLowerCase();
+                        var str2 = rowB.cells[colNum].innerHTML.toLowerCase();
+                        if (str1.charAt(0) == '"') {
+                            str1 = str1.substr(1);
+                        }
+                        if (str2.charAt(0) == '"') {
+                            str2 = str2.substr(1);
+                        }
+                        return str1 > str2 ? 1 : -1;
+                    };
+                    if (document.getElementById('sorted') !== null)document.getElementById('sorted').removeAttribute("id");
+                    grid.tHead.rows[0].cells[colNum].id = 'sorted';
+                }
             break;
         }
 
@@ -86,13 +97,10 @@
  
 
         // добавить результат в нужном порядке в TBODY
-        for(var i=0; i<rowsArray.length; i++) {
+        for(i=0; i<rowsArray.length; i++) {
           tbody.appendChild(rowsArray[i]);
         }
 
         grid.appendChild(tbody);
 
       }
-
-      // P.S. В IE7 cells, cellIndex не работают, если элемент вне документа
-
