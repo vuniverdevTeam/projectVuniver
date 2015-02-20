e6ab94999fcdff220a21d3d598c3603b97081d34
NewGeib <iviviv1901@gmail.com>
2015-02-20 10:24:12 +0200

    Двустороннє сортування(відображення)

--- script/tablesorter/sort.js
+++ script/tablesorter/sort.js
@@ -4,11 +4,9 @@
         var target = e && e.target || window.event.srcElement;
 
         if (target.tagName != 'TH') return;
-
-        // Если TH -- сортируем
-				$('#myList').find('tr:lt('+x+')').attr("hidden", "hidden");
         sortGrid(target.cellIndex, target.getAttribute('data-type'));
-				$('#myList').find('tr:lt('+x+')').removeAttr("hidden");
+          $('#myList').find('tr:lt('+x+')').attr('hidden', 'hidden');
+          $('#myList').find('tr:lt('+x+')').removeAttr('hidden');
       };
 
       function sortGrid(colNum, type) {
@@ -20,83 +18,83 @@
           rowsArray.push(tbody.children[i]);
         }
 
-        // определить функцию сравнения, в зависимости от типа
+        //
         var compare;
 
         switch(type) {
           case 'number':
-            if(document.getElementById('sorted') !== null && document.getElementById('sorted').cellIndex == colNum) {
+            if(document.getElementById('fa-sort-down') !== null && document.getElementById('fa-sort-down').cellIndex == colNum) {
                 compare = function (rowA, rowB) {
                     var a = rowA.cells[colNum].innerHTML;
                     var b = rowB.cells[colNum].innerHTML;
-                    if(a.charAt(a.length-1) == '%'){a=a.substring(0, a.length-1);}
-                    if(b.charAt(b.length-1) == '%'){b=b.substring(0, b.length-1);}
+                    if (a.charAt(a.length - 1) == '%') {
+                        a = a.substring(0, a.length - 1);
+                    }
+                    if (b.charAt(b.length - 1) == '%') {
+                        b = b.substring(0, b.length - 1);
+                    }
                     return b - a;
                 };
-                document.getElementById('sorted').removeAttribute("id");
-                //grid.tHead.rows[0].cells[colNum].id = 'sorted';
-            }	else {
+                document.getElementById('fa-sort-down').id = 'fa-sort-up';
+            } else {
                 compare = function (rowA, rowB) {
                     var a = rowA.cells[colNum].innerHTML;
                     var b = rowB.cells[colNum].innerHTML;
-                    if(a.charAt(a.length-1) == '%'){a=a.substring(0, a.length-1);}
-                    if(b.charAt(b.length-1) == '%'){b=b.substring(0, b.length-1);}
+                    if (a.charAt(a.length - 1) == '%') {
+                        a = a.substring(0, a.length - 1);
+                    }
+                    if (b.charAt(b.length - 1) == '%') {
+                        b = b.substring(0, b.length - 1);
+                    }
                     return a - b;
                 };
-                if (document.getElementById('sorted') !== null)document.getElementById('sorted').removeAttribute("id");
-                grid.tHead.rows[0].cells[colNum].id = 'sorted';
+                if (document.getElementById('fa-sort-down') !== null)document.getElementById('fa-sort-down').removeAttribute('id');
+                else if (document.getElementById('fa-sort-up') !== null)document.getElementById('fa-sort-up').removeAttribute('id');
+                grid.tHead.rows[0].cells[colNum].id = 'fa-sort-down';
             }
             break;
           case 'string':
-                if(document.getElementById('sorted') !== null && document.getElementById('sorted').cellIndex == colNum) {
-                    compare = function (rowA, rowB) {
-                        var str1 = rowA.cells[colNum].innerHTML.toLowerCase();
-                        var str2 = rowB.cells[colNum].innerHTML.toLowerCase();
-                        if (str1.charAt(0) == '"') {
-                            str1 = str1.substr(1);
-                        }
-                        if (str2.charAt(0) == '"') {
-                            str2 = str2.substr(1);
-                        }
-                        return str1 > str2 ? -1 : 1;
-                    };
-                    document.getElementById('sorted').removeAttribute("id");
-                }	else {
-                    compare = function (rowA, rowB) {
-                        var str1 = rowA.cells[colNum].innerHTML.toLowerCase();
-                        var str2 = rowB.cells[colNum].innerHTML.toLowerCase();
-                        if (str1.charAt(0) == '"') {
-                            str1 = str1.substr(1);
-                        }
-                        if (str2.charAt(0) == '"') {
-                            str2 = str2.substr(1);
-                        }
-                        return str1 > str2 ? 1 : -1;
-                    };
-                    if (document.getElementById('sorted') !== null)document.getElementById('sorted').removeAttribute("id");
-                    grid.tHead.rows[0].cells[colNum].id = 'sorted';
-                }
+              if(document.getElementById('fa-sort-down') !== null && document.getElementById('fa-sort-down').cellIndex == colNum) {
+                  compare = function (rowA, rowB) {
+                      var str1 = rowA.cells[colNum].innerHTML.toLowerCase();
+                      var str2 = rowB.cells[colNum].innerHTML.toLowerCase();
+                      if (str1.charAt(0) == '"') {
+                          str1 = str1.substr(1);
+                      }
+                      if (str2.charAt(0) == '"') {
+                          str2 = str2.substr(1);
+                      }
+                      return str1 > str2 ? -1 : 1;
+                  };
+                  document.getElementById('fa-sort-down').id = 'fa-sort-up';
+              }	else {
+                  compare = function (rowA, rowB) {
+                      var str1 = rowA.cells[colNum].innerHTML.toLowerCase();
+                      var str2 = rowB.cells[colNum].innerHTML.toLowerCase();
+                      if (str1.charAt(0) == '"') {
+                          str1 = str1.substr(1);
+                      }
+                      if (str2.charAt(0) == '"') {
+                          str2 = str2.substr(1);
+                      }
+                      return str1 > str2 ? 1 : -1;
+                  };
+                  if (document.getElementById('fa-sort-down') !== null)document.getElementById('fa-sort-down').removeAttribute('id');
+                  else if (document.getElementById('fa-sort-up') !== null)document.getElementById('fa-sort-up').removeAttribute('id');
+                  grid.tHead.rows[0].cells[colNum].id = 'fa-sort-down';
+              }
             break;
         }
-
-        // сортировать
 				
         rowsArray.sort(compare);
 
-        // Убрать tbody из большого DOM документа для лучшей производительности
         grid.removeChild(tbody);
-				
-        // Убрать TR из TBODY.
-        // Присваивание tbody.innerHTML = '' не работает в IE
-        // 
-        // на самом деле без этих строк можно обойтись! 
-        // при добавлении appendChild все узлы будут сами перемещены на правильное место!
+
+        //tbody.innerHTML = '' не працює IE
         while(tbody.firstChild) {
           tbody.removeChild(tbody.firstChild);
         }
- 
 
-        // добавить результат в нужном порядке в TBODY
         for(i=0; i<rowsArray.length; i++) {
           tbody.appendChild(rowsArray[i]);
         }
